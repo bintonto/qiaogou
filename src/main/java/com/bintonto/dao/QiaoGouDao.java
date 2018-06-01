c9cf057a68c56c07e59f04b4cdd89a3af758aef2
bintonto <bintonto@163.com>
2018-06-01 21:33:41 +0800

    抢购业务dao层编写完成

--- src/main/java/com/bintonto/dao/QiaoGouDao.java
+++ src/main/java/com/bintonto/dao/QiaoGouDao.java
@@ -1,17 +1,35 @@
 package com.bintonto.dao;
 
+import com.bintonto.entity.QiangGou;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public interface QiaoGouDao {
     /**
      * 减库存
-     * @param seckillId
-     * @param killTime
      * @return 返回1 表示更新行数， 0 表示失败
      */
-    int reduceNumber(@Param("seckillId") long seckillId, @Param("killTime") Date killTime);
+    int reduceNumber(@Param("qiangGouId") long qiangGouId, @Param("killTime") Date killTime);
+
+    /**
+     * 查询单个秒杀对象
+     */
+    QiangGou queryById(long qiangGouId);
+
+    /**
+     * 根据偏移查询多个对象
+     * 多个参数需要告诉mybatis
+     */
+    List<QiangGou> queryAll(@Param("offset") int offset, @Param("limit") int limit);
+
+    /**
+     * 使用存储过程执行秒杀
+     * @param paramMap
+     */
+    void killByProcedure(Map<String, Object> paramMap);
 
 
 }
